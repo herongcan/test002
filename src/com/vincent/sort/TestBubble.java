34d0df3f02e162aabd71c4bd1c7a65b03c16d14f
Steven <steven@itheima.com>
2018-05-07 10:10:06 +0800

    zz

--- src/com/vincent/sort/TestBubble.java
+++ src/com/vincent/sort/TestBubble.java
@@ -14,9 +14,6 @@ public class TestBubble {
 
     private static void myBubbleSort(int[] arrs){
         //内层控制轮数
-//        for (int j = 0; j<arrs.length-1;j++) {
-//            System.out.println(arrs[j]);
-//        }
 
         //外层控制冒泡索引 4--3--2--1
         for (int i = arrs.length-1;i>0;i--) {
