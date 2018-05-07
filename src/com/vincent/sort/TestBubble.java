84c465bd305b64e8ea85d23eecb99563ee794ff7
Steven <steven@itheima.com>
2018-05-07 10:07:16 +0800

    zz

---
+++ src/com/vincent/sort/TestBubble.java
@@ -0,0 +1,33 @@
+package com.vincent.sort;
+
+/**
+ * created by Administrator on 2018/5/6.
+ */
+public class TestBubble {
+    public static void main(String[] args) {
+        int[] arrs = {1,3,2,6,4};
+        myBubbleSort(arrs);
+        for (int j = 0; j<arrs.length;j++) {
+            System.out.println(arrs[j]);
+        }
+    }
+
+    private static void myBubbleSort(int[] arrs){
+        //内层控制轮数
+//        for (int j = 0; j<arrs.length-1;j++) {
+//            System.out.println(arrs[j]);
+//        }
+
+        //外层控制冒泡索引 4--3--2--1
+        for (int i = arrs.length-1;i>0;i--) {
+            for (int j = 0; j<i;j++) {
+                if (arrs[j]>arrs[j+1]){
+                    int temp = arrs[j];
+                    arrs[j] = arrs[j+1];
+                    arrs[j+1] = temp;
+                }
+            }
+
+        }
+    }
+}
